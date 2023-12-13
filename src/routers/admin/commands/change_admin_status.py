9709186fdf382033928a85367064d54b2c76f613
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-12-13 16:32:15 +0300

    add handler for information on using commands

--- src/routers/admin/commands/change_admin_status.py
+++ src/routers/admin/commands/change_admin_status.py
@@ -5,6 +5,10 @@ from src.common.dto.user import UserUpdate
 from src.common.interfaces.abstract_uow import AbstractUnitOfWork
 from src.routers.admin.router import admin_router
 from src.utils.decorators import with_database
+from src.utils.lexicon import (CANCEL_ADMIN_STATUS_MESSAGE,
+                               INFORMATION_FOR_CHANGE_ADMIN_STATUS_COMMAND,
+                               SET_ADMIN_STATUS_MESSAGE,
+                               USER_NOT_FOUND_MESSAGE)
 
 
 @admin_router.message(F.text.regexp(r'/set_admin_status (\d+)').group(1).as_('user_id'))
@@ -22,9 +26,9 @@ async def process_set_admin_status(
     )
 
     if result:
-        await message.answer(text=f'User {user_id} has been assigned the position of administrator.')
+        await message.answer(text=SET_ADMIN_STATUS_MESSAGE)
     else:
-        await message.answer(text=f'User {user_id} does not exist')
+        await message.answer(text=USER_NOT_FOUND_MESSAGE)
 
 
 @admin_router.message(F.text.regexp(r'/cancel_admin_status (\d+)').group(1).as_('user_id'))
@@ -42,6 +46,13 @@ async def process_cancel_admin_status(
     )
 
     if result:
-        await message.answer(text=f'User {user_id} has been removed as administrator')
+        await message.answer(text=CANCEL_ADMIN_STATUS_MESSAGE)
     else:
-        await message.answer(text=f'User {user_id} does not exist')
+        await message.answer(text=USER_NOT_FOUND_MESSAGE)
+
+
+@admin_router.message(F.text.regexp(r'(\/set_admin_status|\/cancel_admin_status)( .*)?'))
+async def information_for_change_admin_status_command(
+        message: Message,
+):
+    await message.answer(text=INFORMATION_FOR_CHANGE_ADMIN_STATUS_COMMAND)
