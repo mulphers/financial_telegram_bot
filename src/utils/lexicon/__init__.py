889b1fbea932d84bbffb4bf44008b5e0513c4598
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-12-13 16:31:12 +0300

    add admin lexicon

--- src/utils/lexicon/__init__.py
+++ src/utils/lexicon/__init__.py
@@ -1,2 +1,3 @@
+from src.utils.lexicon.admin import *
 from src.utils.lexicon.client import *
 from src.utils.lexicon.command import *
---
+++ src/utils/lexicon/admin.py
@@ -0,0 +1,11 @@
+SET_ADMIN_STATUS_MESSAGE = '✅ Пользователь назначен на пост администратора!'
+
+CANCEL_ADMIN_STATUS_MESSAGE = '❌ Пользователь снят с поста администратора!'
+
+INFORMATION_FOR_CHANGE_ADMIN_STATUS_COMMAND = '''
+⚠️ Информация:
+1. Для назначения пользователя на пост администратора введите /set_admin_status <user_id>
+2.  Для снятия пользователя с поста администратора введите /cancel_admin_status <user_id>
+'''
+
+USER_NOT_FOUND_MESSAGE = '❗️ Ошибка! Пользователя с таким id не существует!'
