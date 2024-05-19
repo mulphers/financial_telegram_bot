fcc4a0b03db83661fc18ec8bb053e6927d301ef8
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-19 16:40:01 +0300

    add new types

--- src/common/types.py
+++ src/common/types.py
@@ -5,6 +5,7 @@ from src.database.models.base.core import Base
 ModelType = TypeVar('ModelType', bound=Base)
 
 SessionType = TypeVar('SessionType')
+TransactionType = TypeVar('TransactionType')
 
 EntryType = TypeVar('EntryType')
 ColumnType = TypeVar('ColumnType')
