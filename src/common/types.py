505b3e3cbaaf8e7d13c2efad428217eef319a51b
Alexander Denisenya <alexanderdenisenya@gmail.com>
2024-05-19 17:15:15 +0300

    add new type

--- src/common/types.py
+++ src/common/types.py
@@ -1,5 +1,7 @@
 from typing import TypeVar
 
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
 from src.database.models.base.core import Base
 
 ModelType = TypeVar('ModelType', bound=Base)
@@ -9,3 +11,5 @@ TransactionType = TypeVar('TransactionType')
 
 EntryType = TypeVar('EntryType')
 ColumnType = TypeVar('ColumnType')
+
+SessionFactoryType = async_sessionmaker[AsyncSession]
